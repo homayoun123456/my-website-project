5d237b458d336f3eef16550dced5b7d42d2bdc97
homayoun123456 <homayoun.farhadi@gmail.com>
2026-07-02 21:21:31 +0330

    feat: add frontend tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        teal: '#14B8A6'
+      },
+      fontFamily: {
+        sans: ['Tahoma', 'Arial', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
